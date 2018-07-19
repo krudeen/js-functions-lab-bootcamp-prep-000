632dd9445a377f0ed2d02f4ab256f5f0e0b3d745
Kyle Rudeen <krudeen@gmail.com>
2018-07-19 17:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ function happyHolidayTo(holiday, name){
 }
 
 
+<<<<<<< HEAD
 function holidayCountdown(holiday, days) {
   return `It\'s ${days} days until ${holiday}!`
+=======
+function holidayCountdown(days, holiday) {
+  return `It\'s ${days} days until ${holiday}`
+>>>>>>> da668d7039293032e03a329ab591b101c4f8eec9
 }
